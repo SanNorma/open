912b1334ee2a0aa28490f123a81b27534dff5a45
Aliaksandr Karankevich <aliaksandr_karankevich@epam.com>
2020-01-22 20:37:48 +0300

    fireStarter.bat added in order to run application, zookeeper and kafka simultaneously

--- magic/fireStarter.bat
+++ magic/fireStarter.bat
@@ -1,10 +1,10 @@
-start cmd.exe /k "gradlew bootRun"
+start cmd.exe /k "gradlew bootRun --args='--verbose:gc;--XX:+HeapDumpOnOutOfMemoryError;--XX:+UseG1GC;--XX:+PrintCommandLineFlags'"
 
 cd c:\SOFT\kafka\
 start cmd.exe /k "zookeeper-server-start.bat config\zookeeper.properties"
 
 start cmd.exe /k "kafka-server-start.bat config\server.properties"
 
-start cmd.exe /k "kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic tradeSystem"
+start cmd.exe /k "kafka-console-consumer.bat --bootstrap-server localhost:9092 --topic tradeSystem --partition 1"
 
-kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic tradeSystem
+kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 2 --topic tradeSystem

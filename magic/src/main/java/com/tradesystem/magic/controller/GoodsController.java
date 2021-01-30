803c721f4e62c505cdcffdfc3383cdec85398b21
Alexander Karankevich <alexander.karankevich@gmail.com>
2021-01-30 20:58:06 +0300

    update a bit (spring boot 2->2.3.6)

--- magic/src/main/java/com/tradesystem/magic/controller/GoodsController.java
+++ magic/src/main/java/com/tradesystem/magic/controller/GoodsController.java
@@ -1,14 +1,12 @@
 package com.tradesystem.magic.controller;
 
-import java.util.List;
-import javax.validation.Valid;
-
 import com.tradesystem.magic.domain.Good;
 import com.tradesystem.magic.domain.projection.NamesDescriptions;
 import com.tradesystem.magic.dto.GoodRequest;
 import com.tradesystem.magic.service.GoodService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(GoodsController.PATH)
+@RequestMapping(value = GoodsController.PATH, produces = "application/json")
 public class GoodsController {
 
     static final String PATH = "/good";
@@ -33,13 +34,13 @@ public class GoodsController {
         return ResponseEntity.ok(goodService.findAll());
     }
 
-    @GetMapping
+    @GetMapping("/{id}")
     public ResponseEntity<Good> getGoodById(@PathVariable Long id) throws NotFoundException {
         var foundGood = goodService.findById(id);
         return ResponseEntity.ok(foundGood);
     }
 
-    @GetMapping
+    @GetMapping("/{id}/description")
     public ResponseEntity<NamesDescriptions> getNamesDescriptionsById(@PathVariable Long id) {
         var nameDescription = goodService.findNameDescriptionById(id);
         return ResponseEntity.ok(nameDescription);
@@ -64,10 +65,5 @@ public class GoodsController {
     public ResponseEntity<Void> deleteGood(Long id) {
         goodService.deleteById(id);
         return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/all")
-    public ResponseEntity<String> getAll() {
-        return ResponseEntity.ok("All");
     }
 }
